b074fd66b59f633a8cee39bd2eaaad72d43343e4
Said LATRACH <said@latrach.net>
2021-09-09 14:42:22 +0200

    merge local and dev

--- docker/openssl/openssl.sh
+++ docker/openssl/openssl.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt"
+echo 'generation des certificats avec opessl'
 # Generation des RootCA à faire qu'une fois si la premiere fois et si pas déja installé en local
 # une fois generer ajouter les certificats localhost.crt et RootCA.crt en les installant:
 # click droit : -> installer -> ordinateur local -> Autorités de certification racine de confiance :
@@ -10,8 +13,6 @@
 
 openssl x509 -req -sha256 -days 1024 -in ../certs/localhost.csr -CA ../certs/RootCA.pem -CAkey ../certs/RootCA.key -CAcreateserial -extfile domains.ext -out ../certs/localhost.crt
 
-#show process in log
-ps -aux
 #keep alive
 echo 'tail -f /dev/null'
 tail -f /dev/null
